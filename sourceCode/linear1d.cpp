d79680ac6ac051aca02ad33b170d69c611253efa
Rafael Campos <rafaelcdn@gmail.com>
2013-12-09 10:44:34 -0200

    biblio e resultados

--- sourceCode/linear1d.cpp
+++ sourceCode/linear1d.cpp
@@ -1,7 +1,7 @@
 float interpolacaoLinear(float x, float *A)
 {
 	const int pisoX = static_cast<int>(floor(x));
-	const int tetoX = static_cast<int>(floor(x));
+	const int tetoX = static_cast<int>(ceil(x));
 	float alpha = x - pisoX;
 	return (1.0 - alpha) * A[pisoX] + alpha * A[tetoX];
 }
---
+++ sourceCode/log.cpp
@@ -0,0 +1,19 @@
+
+Opening file: /vdb_files/smoke.vdb
+VDB file loaded. Volume details: 
+
+creator: SOP_OpenVDB_Write
+Grid name: density
+Grid value type: float
+ class: fog volume
+ file_bbox_max: [111, 223, 112]
+ file_bbox_min: [1, 2, 1]
+ file_mem_bytes: 6984656
+ file_voxel_count: 1049275
+ is_local_space: false
+ is_saved_as_half_float: true
+ name: density
+ roiMax: [112, 224, 113]
+ roiMin: [0, 0, 0]
+ value_type: float
+ vector_type: invariant
